cbb8c6783fbd67e519614e020c6c3c8f51663faf
KronoxYT <keylertamayo@gmail.com>
2026-07-18 02:34:55 -0400

    refactor: remove core project files, assets, and engine configuration scripts

--- Scripts/Enemies/States/EnemyAlertState.gd
+++
@@ -1,9 +0,0 @@
-extends "res://Scripts/Core/State.gd"
-
-func enter(_prev_state = ""):
-    entity.velocity = Vector2.ZERO
-    entity.play_animation("alert")
-
-func physics_process(_delta: float):
-    if entity.ai and entity.ai.current_state != entity.ai.AIState.ALERT:
-        pass
